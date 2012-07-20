2c72030a5b5a7fad8d3e86fdda63bd6a56c13d86
Mihai Zamfirescu <mihai.zamfirescu93@gmail.com>
2012-07-20 13:00:05 +0300

    Trimmed mod_form.php to a friendlier and more usable size

--- mod_form.php
+++ mod_form.php
@@ -31,49 +31,29 @@ defined('MOODLE_INTERNAL') || die();
 
 require_once($CFG->dirroot.'/course/moodleform_mod.php');
 
-/**
- * Module instance settings form
- */
 class mod_feedbackccna_mod_form extends moodleform_mod {
 
-    /**
-     * Defines forms elements
-     */
-    public function definition() {
+    function definition() {
 
+	global $COURSE;
         $mform = $this->_form;
 
-        //-------------------------------------------------------------------------------
-        // Adding the "general" fieldset, where all the common settings are showed
+        
+
+
+	// Adding the "general" fieldset, where all the common settings are showed
         $mform->addElement('header', 'general', get_string('general', 'form'));
 
         // Adding the standard "name" field
         $mform->addElement('text', 'name', get_string('feedbackccnaname', 'feedbackccna'), array('size'=>'64'));
-        if (!empty($CFG->formatstringstriptags)) {
-            $mform->setType('name', PARAM_TEXT);
-        } else {
-            $mform->setType('name', PARAM_CLEAN);
-        }
+        $mform->setType('name', PARAM_TEXT);
         $mform->addRule('name', null, 'required', null, 'client');
         $mform->addRule('name', get_string('maximumchars', '', 255), 'maxlength', 255, 'client');
-        $mform->addHelpButton('name', 'feedbackccnaname', 'feedbackccna');
 
-        // Adding the standard "intro" and "introformat" fields
         $this->add_intro_editor();
 
-        //-------------------------------------------------------------------------------
-        // Adding the rest of feedbackccna settings, spreeading all them into this fieldset
-        // or adding more fieldsets ('header' elements) if needed for better logic
-        $mform->addElement('static', 'label1', 'feedbackccnasetting1', 'Your feedbackccna fields go here. Replace me!');
-
-        $mform->addElement('header', 'feedbackccnafieldset', get_string('feedbackccnafieldset', 'feedbackccna'));
-        $mform->addElement('static', 'label2', 'feedbackccnasetting2', 'Your feedbackccna fields go here. Replace me!');
-
-        //-------------------------------------------------------------------------------
-        // add standard elements, common to all modules
         $this->standard_coursemodule_elements();
-        //-------------------------------------------------------------------------------
-        // add standard buttons, common to all modules
-        $this->add_action_buttons();
+        
+	$this->add_action_buttons();
     }
 }
